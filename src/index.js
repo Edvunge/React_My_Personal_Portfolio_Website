bce2aba5698cb91cd04d1dedbacd295ad4a2114d
edvunge <costaedy18@gmail.com>
2024-06-24 00:32:20 +0100

    Managing and creating files & folders

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom'
+import React from "react";
+import App from './App'
+import './index.css'
+
+
+// ReactDOM.render(<App/>, document.querySelector("#root")) 
+
+const root = ReactDOM.createRoot(document.getElementById("#root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+//reportWebVitals();
