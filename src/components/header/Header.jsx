47cc0b2b356c6b4e06029d36e134e914c291aadd
edvunge <costaedy18@gmail.com>
2024-06-24 22:14:39 +0100

    Installing React Icons and Using them in our project_part1

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import ME from '../../assets/edvaldo-vunge-removebg-preview.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -11,7 +12,7 @@ const Header = () => {
         <h1>Edvaldo Vunge</h1>
         <h5 className="text-light">Java Software Engineer</h5>
         <CTA />
-
+        <HeaderSocials />
         <div className="me">
           <img src={ME} alt="me" />
         </div>
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { FaLinkedin } from "react-icons/fa";
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
         <a href="https://github.com/Edvunge" target="_blank"></a>
-        <a href="https://www.linkedin.com/in/edvaldo-vunge/" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/edvaldo-vunge/" target="_blank"><FaLinkedin/></a>
         <a href="https://dribbble.com/3dv6ld0" target="_blank"></a>
     </div>
   )
