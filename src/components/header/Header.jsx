0aa865943be6b182e2e220f73e2c8a4da51030fe
edvunge <costaedy18@gmail.com>
2024-06-24 22:00:16 +0100

    Header Component JSX

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/edvaldo-vunge-removebg-preview.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Edvaldo Vunge</h1>
+        <h5 className="text-light">Java Software Engineer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://github.com/Edvunge" target="_blank"></a>
+        <a href="https://www.linkedin.com/in/edvaldo-vunge/" target="_blank"></a>
+        <a href="https://dribbble.com/3dv6ld0" target="_blank"></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
