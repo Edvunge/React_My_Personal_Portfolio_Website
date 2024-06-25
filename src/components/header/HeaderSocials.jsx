93842c2556a145594aae46b96df6742a5ce8ef0e
edvunge <costaedy18@gmail.com>
2024-06-25 01:28:53 +0100

    Installing React Icons and Using them in our project

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { FaLinkedin } from "react-icons/fa";
+import { IoLogoGithub } from "react-icons/io";
+import { FaSquareDribbble } from "react-icons/fa6";
 
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://github.com/Edvunge" target="_blank"></a>
+        <a href="https://github.com/Edvunge" target="_blank"><IoLogoGithub/></a>
         <a href="https://www.linkedin.com/in/edvaldo-vunge/" target="_blank"><FaLinkedin/></a>
-        <a href="https://dribbble.com/3dv6ld0" target="_blank"></a>
+        <a href="https://dribbble.com/3dv6ld0" target="_blank"><FaSquareDribbble/></a>
     </div>
   )
 }
