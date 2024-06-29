ba483bf7844f49ad4ea79b89d79dfb8aa429fbcc
edvunge <costaedy18@gmail.com>
2024-06-29 18:59:29 +0100

    Generating Testimonials from Array using map method

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -6,6 +6,30 @@ import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
 
+const data = [
+  {
+    avatar: AVTR1, 
+    name: 'Tina Snow',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam commodi velit porro blanditiis consequatur qui molestiae Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?' 
+  },
+  {
+    avatar: AVTR2, 
+    name: 'Shatta Wale',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur presentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR3, 
+    name: 'Kwame Despite',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur praesentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+  {
+    avatar: AVTR4, 
+    name: 'Nana Ama McBrown',
+    review: 'Modi alias animi dolorem aliquam ea eum beatae maiores, consectetur presentium quibusdam, commodi velit porro blanditiis consequatur qui molestiae. Dolorem, perspiciatis aspernatur labore distinctio ratione delectus voluptatem dolores deserunt explicabo nostrum ducimus quasi?'
+  },
+  ]
+
+
 const Testimonials = () => {
   return (
     <section id='testimonials'>
@@ -13,50 +37,19 @@ const Testimonials = () => {
       <h2>Testimonials</h2>
 
       <div className="container testimonials__container">
-        <article className="testimonial">
-         <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-         </div>
-            <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-              Beatae eos cupiditate nulla doloremque corrupti totam in fugit molestiae rerum nisi, 
-              quisquam, amet culpa nemo facilis, repudiandae tenetur quo quae fugiat?
-            </small>
-        </article>
-        <article className="testimonial">
-         <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-         </div>
-            <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-              Beatae eos cupiditate nulla doloremque corrupti totam in fugit molestiae rerum nisi, 
-              quisquam, amet culpa nemo facilis, repudiandae tenetur quo quae fugiat?
-            </small>
-        </article>
-        <article className="testimonial">
-         <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-         </div>
-            <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-              Beatae eos cupiditate nulla doloremque corrupti totam in fugit molestiae rerum nisi, 
-              quisquam, amet culpa nemo facilis, repudiandae tenetur quo quae fugiat?
-            </small>
-        </article>
-        <article className="testimonial">
-         <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-         </div>
-            <h5 className='client__name'>Ernest Achiever</h5>
-            <small className='client__review'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-              Beatae eos cupiditate nulla doloremque corrupti totam in fugit molestiae rerum nisi, 
-              quisquam, amet culpa nemo facilis, repudiandae tenetur quo quae fugiat?
-            </small>
-        </article>
+        {
+          data.map(({avatar, name, review}, index) => {
+            return (
+                  <article key={index} className="testimonial">
+                  <div className="client__avatar">
+                        <img src={avatar} />
+                  </div>
+                      <h5 className='client__name'>{name}</h5>
+                        <small className='client__review'>{review}</small>
+                  </article>
+            )
+          })
+        }
       </div>
     </section>
   )
