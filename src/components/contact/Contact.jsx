8e533c221f474ed2732f65cd350fb17c8b8385a2
edvunge <costaedy18@gmail.com>
2024-06-29 21:30:00 +0100

    Contact Component CSS

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,19 +13,19 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            <MdOutlineEmail/>
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>ecvunge@protonmail.com</h5>
             <a href="mailto:ecvunge@protonmail.com" target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
-            <RiMessageLine/>
+            <RiMessageLine className='contact__option-icon'/>
             <h4>Messenger</h4>
             <h5>edvaldo vunge</h5>
             <a href="https://m.me/edvaldo.vunge.1" target="_blank">Send a message</a>
           </article>
           <article className="contact__option">
-            <BsWhatsapp/>
+            <BsWhatsapp className='contact__option-icon'/>
             <h4>Whatsapp</h4>
             <h5>+351912270832</h5>
             <a href="https://api.whatsapp.com/send?phone=+351912270832" target="_blank">Send a message</a>
@@ -33,9 +33,9 @@ const Contact = () => {
         </div>
         {/* END OF CONTACT OPTIONS */}
         <form action="">
-          <input type="text" name='name' placeholder='Your Full Name' required/>
-          <input type="email" name='email' placeholder='Your Email' required/>
-          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required ></textarea>
           <button type="submit" className='btn btn-primary'>Send Message</button>
         </form>
       </div>  
