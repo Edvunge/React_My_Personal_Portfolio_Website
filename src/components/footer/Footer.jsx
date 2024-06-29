31e43f02c9dbf2333519f65630a820ba17e27f46
edvunge <costaedy18@gmail.com>
2024-06-29 23:49:38 +0100

    Footer Component CSS

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ import { IoLogoTwitter } from 'react-icons/io'
 const Footer = () => {
   return (
     <footer>
-      <a href="#" className='footer_logo'>EdVunge</a>
+      <a href="#" className="footer__logo">EdVunge</a>
 
       <ul className="permalinks">
         <li><a href="#">Home</a></li>
