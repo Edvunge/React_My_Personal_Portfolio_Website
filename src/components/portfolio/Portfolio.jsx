2b7836221b2cba4a3dcc3da6c61dad0aaad7bbb1
edvaldovunge <costaedy18@gmail.com>
2025-06-30 01:29:52 +0100

    update portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -13,42 +13,42 @@ const data = [
   {
     id: 1, 
     image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization', 
+    title: 'Vitae-AI - Otimização Inteligente de Currículos', 
     github: 'https://github.com',
     demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboard-and-financial-data-visualization'
   },
   {
     id: 2, 
     image: IMG2,
-    title: 'Charts templates & infographics in Figma', 
+    title: 'ChatDocs Saas App', 
     github: 'https://github.com',
     demo: 'https://dribbble.com/shots/16580766-Orion-Ul-kit-Charts-templates-infographics-in-Figma'
   },
   {
     id: 3, 
     image: IMG3,
-    title: 'Figma dashboard Ul kit for data design web apps', 
+    title: 'RECEIPT-AI', 
     github: 'https://github.com',
     demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-Ul-kit-for-data-design-web-apps'
   },
   {
     id: 4, 
     image: IMG4,
-    title: 'Maintaining tasks and tracking progress', 
+    title: 'Voz Interna Blog', 
     github: 'https://github.com',
     demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
   },
   {
     id: 5, 
     image: IMG5,
-    title: 'Charts templates & infographics in Figma', 
+    title: 'Nimbly', 
     github: 'https://github.com',
     demo: 'https://dribbble.com/shots/16541289-Orion-Ul-kit-Charts-templates-infographics-in-Figma'
   },
   {
     id: 6, 
     image: IMG6,
-    title: 'Charts templates & infographics in Figma', 
+    title: 'Vira Clips', 
     github: 'https://github.com',
     demo: 'https://dribbble.com/shots/15887665-Orion-Ul-kit-Charts-templates-infographics-in-Figma'
   }
